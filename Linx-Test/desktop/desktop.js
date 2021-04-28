e78670382f6d79658ff5f7e9712fe6d0b92ca7d7
gabrielmNascimento <82829098+gabrielmNascimento@users.noreply.github.com>
2021-04-28 15:41:32 -0300

    Add files via upload
    Javascript desktop finished with a lot of new things learned, it's a basic code, I'm very proud of me, now let's go to the mobile page, I never made a mobile page before, will be interesting to learn and make one for this challenge.

--- Linx-Test/desktop/desktop.js
+++ Linx-Test/desktop/desktop.js
@@ -1,3 +1,62 @@
-//Getting API Url
-const apiProducts_url = "https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1";
+//Starting API Url
+var apiProducts_url = "https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1";
 
+//to get response from API
+async function getApi(url){
+
+    const response = await fetch(url);
+
+    var data = await response.json();
+
+    show(data);
+}
+
+//Mostrando produtos
+function show(data){
+    for(i = 0; i < data.products.length; i++)
+    {
+        let showProduct =
+        `<div class="info-produto">
+            <div class="image-product">
+                <img src="${data.products[i].image}" alt="" width="200px" height="150px">
+            </div>
+            <div class="name-product">
+                <h4>${data.products[i].name}</h4>
+            </div>
+            <div class="description-product">
+                <p>${data.products[i].description}</p>
+            </div>
+            <div class="old-price">
+                <p>De: R$${data.products[i].oldPrice}</p>
+            </div>
+            <div class="new-price">
+                <p>Por: R$${data.products[i].price}</p>
+            </div>
+            <div class="divided-price">
+                <p>ou ${data.products[i].installments.count}x de ${data.products[i].installments.value}</p>
+            </div>
+
+            <button class="buy-button">Comprar</button>
+        </div>`
+
+        document.getElementById('list-product').innerHTML += showProduct;
+    }
+
+    apiProducts_url = `https://${data.nextPage}`;
+}
+
+//Calling function when pages load:
+console.log(getApi(apiProducts_url));
+
+
+/*Checking if the name has only white spaces, with email we don't need because submit would block if the value in
+text box doesn't correspond to the type of the input.*/
+function checkNameFriend(){
+
+    const nameBox = document.getElementById('name-friend').value;
+
+    if (!nameBox.replace(/\s/g, '').length) {
+        alert("Insira um nome por gentileza");
+        return false;
+    }
+}
